875ac26be3f0b9af04526999084bc983b694315d
Irene Ortega <72819188+irene-op@users.noreply.github.com>
2023-03-31 08:21:02 +0200

    append img to div fixed and working now

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-let form = document.querySelector("#add-meme-form");
+const form = document.getElementById('add-meme-form');
+
+
+
 
 function addMeme (event){
     event.preventDefault();
-    let imageURL = document.querySelector("#image-url");
-    //console.log(`hi there ${imageURL.value}`);
-     
-    const addMemeSection = document.querySelector("#add-memes-section");
-    
-    const newMeme = document.createElement("img");
-    newMeme.setAttribute("src", `${imageURL.value}`);
 
-    //const imgContentDiv = document.createElement("div");
-    //imgContentDiv.classList.add("img-content");
-    // need to create a div as a parent to the img and the text. The div and img would have absolute positioning and the text relative 
-    //document.body.append(newMeme);
-    addMemeSection.appendChild("newMeme");
+    const imageURL = document.getElementById('image-url');
+    const addedMemes = document.getElementById('added-memes');
+    const textTop = document.getElementById('text-top');
+    const textTopValue = textTop.value;
+    const textBottom = document.getElementById('text-bottom');
+    const textBottomValue = textBottom.value;
+    
+    const newMeme = document.createElement('img');
+    newMeme.setAttribute('src', `${imageURL.value}`);
+
+    addedMemes.appendChild(newMeme);
 }
 
-form.addEventListener("submit", addMeme);
+form.addEventListener('submit', addMeme);
 
-// function deleteMeme (event){
-//     event.preventDefault();
-//     image.remove();
-    
-// }
 
 document.addEventListener('click', function(e) {
-  if (e.target.tagName === 'IMG') {
-    e.target.remove();
-  }
+if (e.target.tagName === 'IMG') {
+e.target.remove();
+}
 });
